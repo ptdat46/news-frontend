8d31663bccf3bc1bcc1f4d475b45d1bf4da6ef7e
ptdat46 <ptdat46work@gmail.com>
2025-04-08 15:28:09 +0700

    init react frontend

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get('/newsapi/v2/top-headlines?country=us&apiKey=YOUR_KEY')
+      .then(res => setArticles(res.data.articles))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Tin Nóng Hôm Nay 🔥</h1>
+      <ul>
+        {articles.map((a, i) => (
+          <li key={i}>{a.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+

6bc59bcbf4f93fc3d043a7228ed824ecf39b3027
ptdat46 <ptdat46work@gmail.com>
2025-04-09 08:58:02 +0700

    api local

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    axios.get('/newsapi/v2/top-headlines?country=us&apiKey=0bdbc07dee6f44179109de04121e1d63')
+    axios.get('/')
       .then(res => {
         setArticles(res.data.articles)
         console.log(res.data.articles);
